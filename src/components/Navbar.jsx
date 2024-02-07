c9ca2c3a9f7b6f290cf678c2c9adc86fcfa69b4c
GOURAV <21cs3019@rgipt.ac.in>
2024-02-08 01:31:25 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css'; // Import CSS file for styling
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="navbar-logo">
+                <div className="logo-text">CodePen</div>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
